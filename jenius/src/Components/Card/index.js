b962f1b41069ab347395c992a72f8b86473e92a5
Wisnu Gautama <ngurahwisnugautama@gmail.com>
2019-01-25 22:57:22 +0700

    refactor style on card

--- jenius/src/Components/Card/index.js
+++ jenius/src/Components/Card/index.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { View,Text, Image, TouchableOpacity } from 'react-native';
+import styles from './style';
 
 class Card extends Component {
     render () {
         return (
-            <TouchableOpacity style={{ flexDirection: 'row', borderWidth: 0.6, borderColor: '#ddd', marginBottom: 10, alignItems: 'center', height: 60 }}>
+            <TouchableOpacity style={styles.wrapperStyle}>
                 <Image 
                    source={{ uri: this.props.avatar }}
-                   style={{ width: 40, height: 40, borderRadius: 20, marginHorizontal: 15 }} />
+                   style={styles.imageStyle} />
                 <Text>{this.props.name}</Text>
             </TouchableOpacity>
         )
--- jenius/src/Components/Card/style.js
+++ jenius/src/Components/Card/style.js
@@ -1,5 +1,18 @@
 import { StyleSheet, Dimensions } from 'react-native';
 
 export default StyleSheet.create({
-    
+    wrapperStyle: { 
+        flexDirection: 'row', 
+        borderWidth: 0.6, 
+        borderColor: '#ddd', 
+        marginBottom: 10, 
+        alignItems: 'center', 
+        height: 60 
+    },
+    imageStyle: { 
+        width: 40, 
+        height: 40, 
+        borderRadius: 20, 
+        marginHorizontal: 15 
+    }
 })
