d3ebad29ecc57506aeee27fd03ba4f4f05f474a7
Wisnu Gautama <ngurahwisnugautama@gmail.com>
2019-01-25 23:38:30 +0700

    handle delete and handle update

--- jenius/src/Screens/Detail/index.js
+++ jenius/src/Screens/Detail/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput } from 'react-native';
+import { View, Text, TextInput, Alert } from 'react-native';
 import CustomButton from '../../Components/CustomButton';
 import styles from './style';
 
@@ -17,6 +17,48 @@ class Detail extends Component {
 
     state = { name: `${this.props.navigation.state.params.user.first_name} ${this.props.navigation.state.params.user.last_name}`, job: '' }
 
+    handleDelete = () => {
+        // the api just response 204
+        const api = `https://reqres.in/api/users/${this.props.navigation.state.params.user.id}`
+        fetch(api, {
+            method: 'DELETE'
+        })
+            .then((response) => {
+                console.log(response)
+                this.props.navigation.navigate('HomeScreen')
+                Alert.alert('Alert', 'user deleted successfully')
+            })
+            .catch((err) => {
+                Alert.alert('Alert', err)
+            })
+    }
+
+    handleUpdate = () => {
+        let { name, job } = this.state
+        const api = `https://reqres.in/api/users/${this.props.navigation.state.params.user.id}`
+        const headers = {
+            'Content-Type': 'application/json'
+        }
+        const body = {
+            name: name,
+            job: job
+        }
+
+        fetch(api, {
+            method: 'PUT',
+            headers: headers,
+            body: JSON.stringify(body)
+        })
+            .then((response) => response.json())
+            .then((responseJSON) => {
+                this.props.navigation.navigate('HomeScreen')
+                Alert.alert('Alert', 'user updated successfully')
+            })
+            .catch((err) => {
+                Alert.alert('Alert', err)
+            })
+    }
+
     render() {
         return (
             <View style={{ flex: 1, backgroundColor: 'white' }}>
@@ -33,11 +75,13 @@ class Detail extends Component {
                 <View style={{ flex: 1, justifyContent: 'flex-end', marginBottom: 50 }}>
                     <View style={{ marginBottom: 20 }}>
                         <CustomButton
-                            title="UPDATE" />
+                            title="UPDATE"
+                            onPress={this.handleUpdate} />
                     </View>
                     <CustomButton
                         title="DELETE"
-                        color="#B73F3A" />
+                        color="#B73F3A"
+                        onPress={this.handleDelete} />
                 </View>
             </View>
         )
