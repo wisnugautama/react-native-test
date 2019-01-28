56af08cbe70406f7bb217b0b440a07e022b4b3b5
Wisnu Gautama <ngurahwisnugautama@gmail.com>
2019-01-28 20:27:21 +0700

    add user redux on home

--- jenius/src/Screens/Home/index.js
+++ jenius/src/Screens/Home/index.js
@@ -2,6 +2,29 @@ import React, { Component } from 'react';
 import { View, Text, Image, TouchableOpacity, ScrollView } from 'react-native';
 import ImagePath from '../../Config/image';
 import Card from '../../Components/Card';
+import { connect } from 'react-redux';
+import { getUserPending, getUserSuccess, getUserFailed } from '../../Store/actions/UserAction';
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        pendingGetUser: () => {
+            dispatch(getUserPending(null))
+        },
+        successGetUser: (user) => {
+            dispatch(getUserSuccess(user))
+        },
+        failedGetUser: (error) => {
+            dispatch(getUserFailed(error))
+        }
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        auth: state.auth,
+        user: state.user
+    }
+}
 
 class Home extends Component {
     static navigationOptions = {
@@ -18,6 +41,7 @@ class Home extends Component {
     state = { users: [], page: 1 }
 
     handleGetUsers = () => {
+        this.props.pendingGetUser()
         let { page, users } = this.state
         const api = `https://reqres.in/api/users?page=${page}`
         const headers = {
@@ -35,11 +59,15 @@ class Home extends Component {
                 for (let i = 0; i < responseJSON.data.length; i++) {
                     this.state.users.push(responseJSON.data[i])
                 }
-
+                this.props.successGetUser(users)
                 this.setState({
                     users: users,
                     page: page
                 })
+            })
+            .catch((err) => {
+                this.props.failedGetUser(err)
+                console.log('err message', err)
             })
     }
 
@@ -68,27 +96,28 @@ class Home extends Component {
     }
 
     render() {
+        console.log('ini users', this.props.user)
         return (
             <ScrollView style={{ flex: 1, backgroundColor: 'white' }}>
                 {this.renderComponent()}
-                {this.state.users.length !== 0 && this.state.page !== 5
-                    &&
-                    <View style={{ flexDirection: 'row', flex: 1 }}>
+                <View style={{ flexDirection: 'row', flex: 1 }}>
+                    {this.state.users.length !== 0 && this.state.page !== 5
+                        &&
                         <TouchableOpacity onPress={this.handleGetUsers} style={{ flex: 2, alignItems: 'center', marginRight: 40, marginTop: 20 }}>
                             <Image
                                 source={ImagePath.icon.loading}
                                 style={{ width: 50, height: 50, alignSelf: 'flex-end' }} />
                         </TouchableOpacity>
-                        <TouchableOpacity onPress={() => this.props.navigation.navigate('AddUserScreen')} style={{ flex: 1, marginRight: 20 }}>
-                            <Image
-                                source={ImagePath.icon.add}
-                                style={{ width: 70, height: 70, alignSelf: 'flex-end' }} />
-                        </TouchableOpacity>
-                    </View>
-                }
+                    }
+                    <TouchableOpacity onPress={() => this.props.navigation.navigate('AddUserScreen')} style={{ flex: 1, marginRight: 20 }}>
+                        <Image
+                            source={ImagePath.icon.add}
+                            style={{ width: 70, height: 70, alignSelf: 'flex-end' }} />
+                    </TouchableOpacity>
+                </View>
             </ScrollView>
         )
     }
 }
 
-export default Home
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
